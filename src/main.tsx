35b7e300d948d518af4478ffd2708ba0ddeab783
ilaydahatipoglu9 <ilaydahatipoglu09@gmail.com>
2026-03-11 16:42:14 +0100

    use case hover over

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-if (import.meta.env.DEV) {
-    import("./devtools/useCaseInspector");
-}
+import("./devtools/useCaseInspector");
+
 createRoot(document.getElementById("root")!).render(<App />);
