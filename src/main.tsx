ebf3a7eb3b5fc0d7d5973afee77491a78388981f
ilaydahatipoglu9 <ilaydahatipoglu09@gmail.com>
2026-04-21 17:47:32 +0200

    fixed main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-import("./devtools/useCaseInspector");
+import("./devtools/previewBridge.ts");
 
 createRoot(document.getElementById("root")!).render(<App />);
