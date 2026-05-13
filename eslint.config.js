24b66b5cc5bb182cab55b348fd6d1c8955fde4a5
ilaydahatipoglu9 <ilaydahatipoglu09@gmail.com>
2026-05-13 20:24:03 +0200

    fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import importPlugin from 'eslint-plugin-import'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'src/devtools/**'] },
   {
     extends: [
       js.configs.recommended,
